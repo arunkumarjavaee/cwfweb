4ec8f1eb389b937daa2dcc9a613510c1fca50044
arunkumar01 <arun.kumar01@gspann.com>
2016-10-02 13:09:44 +0530

    initialize

---
+++ src/main/java/com/cwf/common/BasicFlowApplication.java
@@ -0,0 +1,12 @@
+package com.cwf.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicFlowApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cwf/common/BasicFlowApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cwf.common;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicFlowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
